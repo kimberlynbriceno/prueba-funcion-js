90a2b758c3f31cadce05142606a6e4465671ef78
kimberlynbriceno <94151778+kimberlynbriceno@users.noreply.github.com>
2022-02-16 20:31:37 -0500

    create object

---
+++ newobject.js
@@ -0,0 +1,10 @@
+function smarfhone(marca, modelo, costo) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.costo = costo;
+}
+
+let otrosmar = new smarfhone(["motorola", "huawei"], ["g9", "t9"], [1.265, 2.265);
+        //let otrosmar = new smarfhone("huawai", "t9", 2.265);
+        //let secondSmar = new smarfhone("samsung", "j2", 150);
+        console.log(otrosmar)
