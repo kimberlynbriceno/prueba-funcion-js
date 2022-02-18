3b95be5c36cec9225f99e80fecec1b6fa760ca10
kimberlynbriceno <94151778+kimberlynbriceno@users.noreply.github.com>
2022-02-17 20:58:02 -0500

    modyfy

--- newobject.js
+++ newobject.js
@@ -7,6 +7,3 @@ function smarfhone(marca, modelo, costo) {
 let otrosmar = new smarfhone(["motorola", "huawei", "samsung"], ["g9", "t9", "j2"], [1.265, 2.265, 150]);
 //let otrosmar = new smarfhone("huawai", "t9", 2.265);
 //let secondSmar = new smarfhone("samsung", "j2", 150);
-let objects = otrosmar.filter(funcion(smar)) {
-    return smar.marca == [0]
-}
