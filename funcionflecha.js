c834a3518ff45e210fa1b5b94817d12083166305
kimberlynbriceno <94151778+kimberlynbriceno@users.noreply.github.com>
2022-02-20 08:50:07 -0500

    fie flech

---
+++ funcionflecha.js
@@ -0,0 +1,9 @@
+let arepa = parceInt(prompt("a que hora colocaste tu arepa"))
+let time = parceInt(prompt("que hora tienes?"))
+
+function quemar(a, b) {
+    return = a - b
+
+}
+quemar(time, arepa)
+if (quemar == )
