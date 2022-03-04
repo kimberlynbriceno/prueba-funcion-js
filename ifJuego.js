76148f43833fb153c37515cf6c0543e6d4e2ab96
kimberlynbriceno <94151778+kimberlynbriceno@users.noreply.github.com>
2022-03-04 16:52:52 -0500

    modify juego

---
+++ ifJuego.js
@@ -0,0 +1,55 @@
+let eleccionMaquina = Math.floor(Math.random() * (3 - 1)) + 1;
+
+var eleccionJugador = 0
+
+function jugar() {
+    if (eleccionMaquina === eleccionJugador) {
+        alert("OH! EMPATE, Quieres volver a intentar?")
+    } else {
+        if (eleccionMaquina === 1 && eleccionJugador === 2) {
+            alert("Piedra")
+            document.getElementById("titulo").innerHTML = "en hora buena !GANASTE!"
+        }
+        if (eleccionMaquina === 1 && eleccionJugador === 3) {
+            alert("Piedra")
+            document.getElementById("titulo").innerHTML = "OH NO! te han derrotaron"
+        }
+        if (eleccionMaquina === 2 && eleccionJugador === 1) {
+            alert("Papel")
+            document.getElementById("titulo").innerHTML = "OH NO! te han derrotaron"
+        }
+        if (eleccionMaquina === 2 && eleccionJugador === 3) {
+            alert("Papel")
+            document.getElementById("titulo").innerHTML = "en hora buena !GANASTE!"
+        }
+        if (eleccionMaquina === 3 && eleccionJugador === 1) {
+            alert("tijera")
+            document.getElementById("titulo").innerHTML = "en hora buena !GANASTE!"
+        }
+        if (eleccionMaquina === 3 && eleccionJugador === 2) {
+            alert("tijera")
+            document.getElementById("titulo").innerHTML = "OH NO! te han derrotaron"
+        }
+
+    }
+}
+
+function piedra() {
+
+    eleccionJugador = 1
+}
+
+
+
+function papel() {
+    eleccionJugador = 2
+}
+
+
+function tijera() {
+    eleccionJugador = 3
+}
+
+jugar()
+
+//alert(eleccionMaquina)
