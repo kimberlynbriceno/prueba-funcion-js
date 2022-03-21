04fcb079798b953c6f1f40874607ccc03dbe2a7f
kimberlynbriceno <94151778+kimberlynbriceno@users.noreply.github.com>
2022-03-21 18:52:03 -0500

    createElement

--- ejercicoDom.js
+++ ejercicoDom.js
@@ -1,15 +1,58 @@
-const inputColor = document.querySelector("#inputColor")
-const boton = document.querySelector("#boton")
-const cardColor = document.querySelector("#cardColor")
-const textoHexa = document.querySelector("#textoHexa")
+const lista = document.querySelector("#listaId")
 
 
+const estados = ["merida", "tachira", "yaracuy", "maracaibo", "trujillo"]
 
-console.log(inputColor.value)
+const fragment = new DocumentFragment();
 
-boton.addEventListener("click", () => {
+estados.forEach((estado) => {
+    const li = document.createElement("li")
+    li.className = "list"
 
-    // textoHexa.textContent = inputColor.value;
+    const blacki = document.createElement('b')
+    blacki.textContent = "pais:"
 
-    // cardColor  = inputColor
+    const text = document.createElement('span')
+    text.className = "text-primary"
+    text.textContent = estado
+
+    li.appendChild(blacki)
+    li.appendChild(text)
+    fragment.appendChild(li)
+
 })
+
+lista.appendChild(fragment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputColor = document.querySelector("#inputColor")
+// const boton = document.querySelector("#boton")
+// const cardColor = document.querySelector("#cardColor")
+// const textoHexa = document.querySelector("#textoHexa")
+// 
+// 
+// 
+// 
+// boton.addEventListener("click", () => {
+// 
+// textoHexa.textContent = inputColor.value;
+// 
+// cardColor.style.backgroundColor = inputColor.value;
+// })
