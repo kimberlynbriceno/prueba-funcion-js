7e8ce666656d5dac85b1ad0cbea33c9fd737aceb
kimberlynB <kimberlynbriceno22@gmail.com>
2023-01-04 20:01:42 -0500

    practica

--- ejerciciosFetch.js
+++ ejerciciosFetch.js
@@ -1,1 +1,35 @@
-console.log("act")
+const cards = document.querySelector("#cardDinamica")
+
+ const template = document.querySelector("#templateCard").content
+ 
+ const loading = document.querySelector("#loadingId")
+
+
+ document.addEventListener('DOMContenLoaded', () => {
+        fetchData()
+ })
+
+ const fetchData = async () => {
+    // console.log("nosequedice")
+    try{
+        loadingData(true)
+    }
+    catch(err){
+
+    }
+    finally{
+        loadingData(false)
+    }
+ }
+
+ const loadingData = (estado) => {
+
+    if(estado){
+        loading.classList.remove(d-none) 
+    }
+    else{
+        loading.classList.add(d-none) 
+
+    }
+ }
+
